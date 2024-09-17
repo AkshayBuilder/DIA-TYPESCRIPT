00381374548f7a9f73608c2644ccc17919bfcd1f
AkshayBuilder <akshaykadukkankimmeehal@gmail.com>
2024-09-17 06:12:25 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 'use client'
 import './globals.css';
 import type { Metadata } from 'next';
-import { Toaster } from 'sonner';
 import { cn } from '@/lib/utils';
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'; // Adjust the path based on your project structure
 import { ReactNode } from 'react';
-import Image from 'next/image';
+import Loading from './loading';
+import { SearchComponent } from '@/components/search';
 
  const metadata: Metadata = {
-  title: 'Book Inventory — Next.js App Router',
-  description: 'View 2 million books from Goodreads.',
+  title: 'Workshop Diagnal',
+  description: 'Movie Listing.',
 };
 
 export default function RootLayout({
@@ -27,10 +27,18 @@ export default function RootLayout({
           'bg-[#171717] text-[#ffffff] font-titillium antialiased'
         )}
       > 
-        <div className="group flex w-full">
+         <div className="group flex w-full">
           
-          <div className="flex-1 flex flex-col min-h-screen">
-            <div className="flex-1 flex flex-col p-1">{children}</div>
+            
+          <div className="flex-1 flex flex-col min-h-screen ">
+            <div className="sticky top-0 z-10  dark:bg-black ">
+              <div className=" py-4 ">
+                <Suspense fallback={<Loading />}>
+                  <SearchComponent />
+                </Suspense>
+              </div>
+            </div>
+            <div className="flex-1 flex flex-col p-4">{children}</div>
           </div>
         </div>
         
