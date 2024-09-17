9a3eda0339e242519f18b3c39e2866efa2a56e49
AkshayBuilder <akshaykadukkankimmeehal@gmail.com>
2024-09-17 00:20:42 -0700

    Update loadMore.tsx

--- src/components/loadMore.tsx
+++ src/components/loadMore.tsx
@@ -109,7 +109,7 @@ const MovieLink = ({
         />
         <span className="absolute bottom-0 left-0 p-2 text-sm font-semibold"></span>
       </Link>
-       <div className="p-2">
+       <div className="p-1">
       {movie.name.length > 12 ? `${movie.name.slice(0,12)}...` : movie.name}
       </div>
     </div>
