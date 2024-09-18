016ca9735cc8248412368e7bf84945c767a7c1c6
AkshayBuilder <akshaykadukkankimmeehal@gmail.com>
2024-09-17 18:58:02 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
                 </Suspense>
               </div>
             </div>
-            <div className="flex-1 flex flex-col p-4">{children}</div>
+            <div className="flex-1 flex flex-col ">{children}</div>
           </div>
         </div>
         
