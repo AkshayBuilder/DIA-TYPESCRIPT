1ab4d90ca9e9441e6af92e0b69fa2f553b593695
AkshayBuilder <akshaykadukkankimmeehal@gmail.com>
2024-10-04 08:14:54 -0700

    Update loadMore.tsx

--- src/components/loadMore.tsx
+++ src/components/loadMore.tsx
@@ -112,7 +112,7 @@ export function LoadMore({ initialMovies }: { initialMovies: Movie[] | null }) {
 
         {/* Spinner for pagination */}
         <div ref={ref}>
-          {pagesLoaded < 4 && <Spinner />}
+          {pagesLoaded < 4 ? <Spinner />:<div></div>}
         </div>
       </div>
     
